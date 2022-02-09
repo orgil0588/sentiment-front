e2ae2f5de80948d8c615321bfbd7073f785b55fe
orgil0588 <76672549+orgil0588@users.noreply.github.com>
2022-02-09 19:56:40 +0800

    some

--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-function App() {
-  setInterval(() => {
-    axios(
-      "https://fx-sentimen.herokuapp.com/"
-    )
+const useMountEffect = () =>
+  useEffect(() => {
+    axios("https://fx-sentimen.herokuapp.com/")
       .then((res) => {
         console.log(res);
       })
       .catch((err) => {
         console.log(err);
       });
-  }, 3600000);
+  }, []);
 
-  return <div className="App"></div>;
+function App() {
+  const [data, setData] = useState("");
+  useMountEffect();
+  useEffect(() => {
+    const interval = setInterval(() => {
+      axios("https://fx-sentimen.herokuapp.com/")
+        .then((res) => {
+          setData(res.data);
+          console.log(res.data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }, 3600000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="App">
+      <pre>{JSON.stringify(data, undefined, 2)}</pre>
+    </div>
+  );
 }
 
 export default App;
