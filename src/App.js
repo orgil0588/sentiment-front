8c71b799baddaa785a0de74cc48a6244da919c5e
orgil0588 <76672549+orgil0588@users.noreply.github.com>
2022-03-10 15:53:49 +0800

    chart fix

--- src/App.js
+++ src/App.js
@@ -1,98 +1,92 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import {
-  LineChart,
-  XAxis,
-  Tooltip,
-  CartesianGrid,
-  Line,
-  YAxis,
-  Legend,
-} from "recharts";
+import Chart from "./Chart";
 
-function App() {
-  const [calcData, setCalcData] = useState("");
-  const [rawData, setRawData] = useState("");
+const App = () => {
   const [data, setData] = useState("");
-  let left = [];
-  let right = [];
+
   useEffect(() => {
-    axios("http://68.183.176.100:8001/api/v1/last-data")
+    axios
+      // .get("http://68.183.176.100:8001/api/v1/history")
+      .get("http://localhost:8001/api/v1/history")
       .then((res) => {
-        setCalcData(res.data.data);
-        setRawData(res.data.rawData);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-    axios("http://68.183.176.100:8001/api/v1/last-data/chart")
-      .then((res) => {
-        let resData = [];
-
-        res.data.data.map((e) => {
-          let obj = {
-            name: e.date,
-            aud: e.value.aud !== undefined ? e.value.aud * 2 : undefined,
-            cad: e.value.cad !== undefined ? e.value.cad * 2 : undefined,
-            chf: e.value.chf !== undefined ? e.value.chf * 2 : undefined,
-            eur: e.value.eur !== undefined ? e.value.eur * 2 : undefined,
-            gbp: e.value.gbp !== undefined ? e.value.gbp * 2 : undefined,
-            jpy: e.value.jpy !== undefined ? e.value.jpy * 2 : undefined,
-            nzd: e.value.nzd !== undefined ? e.value.nzd * 2 : undefined,
-            usd: e.value.usd !== undefined ? e.value.usd * 2 : undefined,
-          };
-          resData.push(obj);
-        });
-
-        setData(resData);
+        setData(res.data.data);
       })
       .catch((err) => {
         console.log(err);
       });
   }, []);
-
-  console.log(data);
-
   return (
-    <div className="mt-10 ">
-      <div className="mx-auto">
-        <LineChart
-          width={1600}
-          height={580}
-          data={data}
-          margin={{ top: 5, right: 20, left: 10, bottom: 5 }}
-        >
-          <XAxis dataKey="name" padding={{ left: 30, right: 30 }} />
-          <YAxis dataKey={data.value} />
-
-          <Tooltip />
-          <Legend />
-
-          <Line type="monotone" dataKey="aud" stroke="#2176AE" />
-          <Line type="monotone" dataKey="cad" stroke="#57B8FF" />
-          <Line type="monotone" dataKey="chf" stroke="#B66D0D" />
-          <Line type="monotone" dataKey="eur" stroke="#FBB13C" />
-          <Line type="monotone" dataKey="gbp" stroke="#FE6847" />
-          <Line type="monotone" dataKey="jpy" stroke="#B3001B" />
-          <Line type="monotone" dataKey="nzd" stroke="#262626" />
-          <Line type="monotone" dataKey="usd" stroke="#BC9CB0" />
-        </LineChart>
+    <div className="">
+      <div className="absolute z-50 top-10 left-10">
+        <div className="flex items-center space-x-2 my-4">
+          <div
+            className="w-8 h-8 rounded"
+            style={{ backgroundColor: "#2C73D2" }}
+          ></div>
+          <p className="font-medium text-white">AUD</p>
+        </div>
+        <div className="flex items-center space-x-2 my-4">
+          <div
+            className="w-8 h-8 rounded"
+            style={{ backgroundColor: "#ffffff" }}
+          ></div>
+          <p className="font-medium text-white">CAD</p>
+        </div>
+        <div className="flex items-center space-x-2 my-4">
+          <div
+            className="w-8 h-8 rounded"
+            style={{ backgroundColor: "#16a34a" }}
+          ></div>
+          <p className="font-medium text-white">CHF</p>
+        </div>
+        <div className="flex items-center space-x-2 my-4">
+          <div
+            className="w-8 h-8 rounded"
+            style={{ backgroundColor: "#C25E5E" }}
+          ></div>
+          <p className="font-medium text-white">JPY</p>
+        </div>
+        <div className="flex items-center space-x-2 my-4">
+          <div
+            className="w-8 h-8 rounded"
+            style={{ backgroundColor: "#F9F871" }}
+          ></div>
+          <p className="font-medium text-white">EUR</p>
+        </div>
+        <div className="flex items-center space-x-2 my-4">
+          <div
+            className="w-8 h-8 rounded"
+            style={{ backgroundColor: "#00C9A7" }}
+          ></div>
+          <p className="font-medium text-white">GBP</p>
+        </div>
+        <div className="flex items-center space-x-2 my-4">
+          <div
+            className="w-8 h-8 rounded"
+            style={{ backgroundColor: "#845EC2" }}
+          ></div>
+          <p className="font-medium text-white">USD</p>
+        </div>
+        <div className="flex items-center space-x-2 my-4">
+          <div
+            className="w-8 h-8 rounded"
+            style={{ backgroundColor: "#AD5E00" }}
+          ></div>
+          <p className="font-medium text-white">NZD</p>
+        </div>
+        <div>
+          <div></div>
+          <p></p>
+        </div>
+        <div>
+          <div></div>
+          <p></p>
+        </div>
       </div>
-
-      <pre className="mt-10">{JSON.stringify(calcData, undefined, 2)}</pre>
-      <div>
-        {rawData &&
-          rawData.map((e) => {
-            return (
-              <div className="flex" key={e.pair}>
-                <h1>{e.pair} : </h1>
-                <p> {e.long}</p>
-              </div>
-            );
-          })}
-      </div>
+      <Chart />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Chart.js
@@ -0,0 +1,124 @@
+import React, { useEffect, useRef, useState } from "react";
+import { createChart, CrosshairMode } from "lightweight-charts";
+import axios from "axios";
+const Chart = () => {
+  const chartContainerRef = useRef();
+  const chart = useRef();
+  const resizeObserver = useRef();
+
+  useEffect(() => {
+    chart.current = createChart(chartContainerRef.current, {
+      width: chartContainerRef.current.clientWidth,
+      height: chartContainerRef.current.clientHeight,
+      layout: {
+        backgroundColor: "#131722",
+        textColor: "rgba(255, 255, 255, 0.9)",
+      },
+
+      grid: {
+        vertLines: {
+          color: "#1C202B",
+        },
+        horzLines: {
+          color: "#1C202B",
+        },
+      },
+      crosshair: {
+        mode: CrosshairMode.Normal,
+      },
+      priceScale: {
+        borderColor: "#485c7b",
+      },
+      priceRange: {
+        minValue: 25,
+        maxValue: -25,
+      },
+      timeScale: {
+        borderColor: "#485c7b",
+      },
+    });
+
+    const lineSeriesAud = chart.current.addLineSeries({
+      lineStyle: 0,
+      color: "#2C73D2",
+      lineWidth: 2,
+      crosshairMarkerVisible: false,
+      LastPriceAnimationMode: 2,
+    });
+    const lineSeriesCad = chart.current.addLineSeries({
+      lineStyle: 0,
+      color: "#ffffff",
+      lineWidth: 2,
+    });
+    const lineSeriesChf = chart.current.addLineSeries({
+      lineStyle: 0,
+      color: "#16a34a",
+      lineWidth: 2,
+    });
+    const lineSeriesJpy = chart.current.addLineSeries({
+      lineStyle: 0,
+      color: "#C25E5E",
+      lineWidth: 2,
+    });
+    const lineSeriesEur = chart.current.addLineSeries({
+      lineStyle: 0,
+      color: "#F9F871",
+      lineWidth: 2,
+    });
+    const lineSeriesGbp = chart.current.addLineSeries({
+      lineStyle: 0,
+      color: "#00C9A7",
+      lineWidth: 2,
+    });
+    const lineSeriesUsd = chart.current.addLineSeries({
+      lineStyle: 0,
+      color: "#845EC2",
+      lineWidth: 2,
+    });
+    const lineSeriesNzd = chart.current.addLineSeries({
+      lineStyle: 0,
+      color: "#AD5E00",
+      lineWidth: 2,
+    });
+
+    axios
+      // .get("http://68.183.176.100:8001/api/v1/history")
+      .get("http://localhost:8001/api/v1/history")
+      .then((res) => {
+        console.log(res.data.data.audArr.length)
+        lineSeriesAud.setData(res.data.data.audArr);
+        lineSeriesCad.setData(res.data.data.cadArr);
+        lineSeriesChf.setData(res.data.data.chfArr);
+        lineSeriesJpy.setData(res.data.data.jpyArr);
+        lineSeriesEur.setData(res.data.data.eurArr);
+        lineSeriesGbp.setData(res.data.data.gbpArr);
+        lineSeriesUsd.setData(res.data.data.usdArr);
+        lineSeriesNzd.setData(res.data.data.nzdArr);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  useEffect(() => {
+    resizeObserver.current = new ResizeObserver((entries) => {
+      const { width, height } = entries[0].contentRect;
+      chart.current.applyOptions({ width, height });
+      setTimeout(() => {
+        chart.current.timeScale().fitContent();
+      }, 0);
+    });
+
+    resizeObserver.current.observe(chartContainerRef.current);
+
+    return () => resizeObserver.current.disconnect();
+  }, []);
+
+  return (
+    <div className="">
+      <div className="h-screen" ref={chartContainerRef} />
+    </div>
+  );
+};
+
+export default Chart;
